b3103e3c44eb04d243730c80a67c3ccdedbdb986
Alfredo Filardo <alfredofilardo@gmail.com>
2020-02-13 13:08:50 +0100

    working with lists

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ConditionalSection from './sections/conditional'
+import cars from './assets/cars.json'
 import logo from './logo.svg';
 import './App.css';
 
@@ -64,24 +66,76 @@ class NumberCounter extends Component {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title text="Welcome to React" />
-        <Text
-          arrayOfNumbers={[1, 2, 3]}
-          multiply={(number) => number * 4}
-          number={2}
-          objectWithInfo={{key: 'value'}}
-          text="Hello world!"
-          title={<h1>This is the title</h1>}
-        />
-        <Counter start={100} />
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Title text="Welcome to React" />
+//         <Text
+//           arrayOfNumbers={[1, 2, 3]}
+//           multiply={(number) => number * 4}
+//           number={2}
+//           objectWithInfo={{key: 'value'}}
+//           text="Hello world!"
+//           title={<h1>This is the title</h1>}
+//         />
+//         <Counter start={100} />     
+//       </header>
+//     </div>
+//   );
+// }
+
+// class App extends Component {
+//   render() {
+//     const numbers = [1, 1, 4, 5]
+
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <Title text="Working with lists" />
+//           {numbers.map((number, index) => {
+//             return <p key={index}>The number {number}</p>
+//           })}
+//         </header>
+//       </div>   
+//     )
+//   }
+// }
+
+class CarItem extends Component {
+  render() {
+    const { car, id } = this.props
+
+    return (
+      <li>
+        <p><strong>ID: </strong>{id}</p>
+        <p><strong>Name: </strong>{car.name}</p>
+        <p><strong>Brand: </strong>{car.company}</p>
+      </li>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Title text="Working with object lists" />
+          <ul>
+            {
+              cars.map(car => {
+                return <CarItem key={car.id} id={car.id} car={car} />
+              })
+            }
+          </ul>
+        </header>
+      </div>   
+    )
+  }
 }
 
 export default App;
---
+++ src/sections/conditional.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class LoginButton extends Component {
+    render() {
+        return <button>Log In</button>
+    }
+}
+
+class LogoutButton extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome, user</p>
+                <button>Logout</button>
+            </div>
+        )
+    }
+}
+
+export default class ConditionalSection extends Component {
+    constructor() {
+        super()
+        this.state = { isUserLogged: true }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Conditional Rendering</h4>
+                {this.state.isUserLogged
+                    ? <LogoutButton />
+                    : <LoginButton />
+                }
+            </div>
+        )
+    }
+}
